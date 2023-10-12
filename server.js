19fcf8423616914555b7c9d338d09448528cf299
sctwomey <sctwomey@gmail.com>
2023-10-12 15:57:22 -0700

    Update js file

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(htmlRoutes)
-app.use(apiRoutes)
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}!`)
